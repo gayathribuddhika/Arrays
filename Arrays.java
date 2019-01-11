2e0468f170fc0250f575f00fcb266102f2736c7e
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-11 21:00:43 +0530

    Arrays
    Arrays

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+
+    
+    public static void main(String[] args) {
+        
+        int [] marks = {56,89,69,76,52,97};
+        //System.out.println(marks[2]);
+        int i=0;
+        while(i<6 ){
+            System.out.println(marks[i]);
+            i++;
+        }
+    }
+    
+}
